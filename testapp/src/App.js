8d9fa9841b3da8eb414e45db4ef01d22fb07ca31
utkarsh <utkarshumre@outlook.com>
2024-01-21 20:37:20 +0530

    express app

--- testapp/src/App.js
+++ testapp/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>project for learning kubernetes</p>
+        <p>kubernetes project V3</p>
         <a
           className="App-link"
           href="https://reactjs.org"
